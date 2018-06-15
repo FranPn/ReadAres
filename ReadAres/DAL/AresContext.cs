d57b170594b35fbeddf9ea87fb1ef828264ff6af
Francesco <franpn@gmail.com>
2018-06-16 00:47:46 +0200

    Add project files.

---
+++ ReadAres/DAL/AresContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ReadAres.Models;
+
+namespace ReadAres.DAL
+{
+    public class AresContext : DbContext
+    {
+        public DbSet<AresModel> AresModels { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ReadAres/Models/AresModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReadAres.Models
+{
+    public class AresModel
+    {
+        public int ID { get; set; }
+        public string PIPPO { get; set; }
+        public int PLUTO { get; set; }
+        public string PAPERINO { get; set; }
+    }
+}
